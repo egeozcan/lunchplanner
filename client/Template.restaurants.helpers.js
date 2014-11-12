be124708949fa474d126ebc91afcfd11e3afce66
Ege <Ege@localhost>
2014-11-12 12:38:25 +0100

    sorted restaurants by title

--- client/Template.restaurants.helpers.js
+++ client/Template.restaurants.helpers.js
@@ -1,6 +1,6 @@
 Template.restaurants.helpers({
 	restaurants: function () {
-		return Collections.Restaurants.find({active: true});
+		return Collections.Restaurants.find({active: true}, { sort: { title: 1 } });
 	},
 	hasRestaurants: function() {
 		return Collections.Restaurants.find().count() > 0;
